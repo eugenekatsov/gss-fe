005e20f9cc293151268e447f45c892280694c008
Eugene Katsov <metabola@gmail.com>
2023-06-30 12:47:53 -0700

    working basic

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # gss-fe
-Front end for testing gss (go sample service)
+Front end for testing gss [go sample service](https://github.com/eugenekatsov/gss)
+
+The frontend and backend are located in individual pods. Nginx on the front end serves up the react page with requests to the backend being proxied to the backend's internal service name.
+
+This is meant to be a quick way to prototype any frontend and backend code with the benefit of being run in an albeit reduced k8s cluster.
+
+## Setup
+
+You'll need the following:
+* I use docker desktop [docker]()
+* to run commands against your cluster [kubectl]()
+* for local kubernetes [minikube]()
+* Charts are a great way to deploy and manage releases to your minikube cluster [helm]()
+* to watch pod logs [stern](https://github.com/stern/stern)
+
+
+## Getting Started
+
+1. Clone this repo
+2. Docker build and tag, then push to docker hub
+4. Update the repository name and tag in the helm chart
+5. kubectl create secret in our case it's regcred see here https://kubernetes.io/docs/tasks/configure-pod-container/pull-image-private-registry/
+6. Helm install from the root repo directory
+7. Verify the pods are up and healthy: `kubectl get pods` or `describe pods`
+8. Run `minikube service <YOUR SERVICE NAME> --url` to get the url
+9. Use postman or curl to verify things are working by getting/posting to the url
+10. To see some tracing run the docker container (on its own not in minikube), exec into it and `cat traces.txt`
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,29 +1,158 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    background: 'linear-gradient(to right, red, blue, yellow, green)',
+  },
+  input: {
+    margin: theme.spacing(1),
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 const App = () => {
-  const [healthStatus, setHealthStatus] = useState('');
+  const classes = useStyles();
+  const [name, setName] = useState('');
+  const [id, setId] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
 
-  const handleHealthClick = () => {
-    // Make the API call to the backend service
-    fetch('/healthz', {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Update the health status state with the response
-        setHealthStatus(data.status);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        // Handle any error that occurred during the API call
-        setHealthStatus('Error');
-      });
+  const getRequest = (mthd, data) => {
+    data.id = parseInt(data.id);
+    
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    let request = {
+      method: mthd,
+      headers: headers,
+      redirect: 'follow'
+    };
+
+    if (mthd == 'GET') return request
+    
+    request.body = JSON.stringify(data);
+
+    return request;
+  };
+
+  const handleCreateUser = async () => {
+    const userData = { name, id };
+    
+    // Make API call to create user using userData
+
+    const request = getRequest('POST', userData);
+
+    await fetchAndDisplay('Create User', '/users', request);
+  };
+
+  const handleUpdateUser = async () => {
+    const userData = { name, id };
+    
+    const request = getRequest('PUT', userData);
+
+    await fetchAndDisplay('Update User', `/users/${id}`, request);
+  };
+
+  const handleGetUser = async () => {
+    const userData = { name, id };
+    
+    const request = getRequest('GET', userData);
+
+    await fetchAndDisplay('Get User', `/users/${id}`, request);
+  };
+
+  const handleDeleteUser = async () => {
+    const userData = { name, id };
+    
+    const request = getRequest('DELETE', userData);
+
+    await fetchAndDisplay('Delete User', `/users/${id}`, request);
+  };
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+
+  const fetchAndDisplay = async (message, endpoint, requestOptions) => {
+    try {
+      const response = await fetch(endpoint, requestOptions);
+      const jsonData = await response.json();
+      console.log("res is: ", jsonData);
+      setSnackbarMessage(`Success! User: ${jsonData.name}`);
+      setSnackbarOpen(true);
+    } catch (e) {
+      console.log(message, e);
+    } 
+    delete requestOptions.body 
+    console.log(message, id);
   };
 
   return (
-    <div>
-      <button onClick={handleHealthClick}>health</button>
-      <p>Health status: {healthStatus}</p>
+    <div className={classes.root}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        className={classes.input}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Id"
+        value={id}
+        className={classes.input}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        onClick={handleCreateUser}
+      >
+        Create User
+      </Button>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        message={snackbarMessage}
+      />
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        onClick={handleUpdateUser}
+      >
+        Update User Only Id
+      </Button>
+      <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        onClick={handleGetUser}
+      >
+        Get User Only Id
+      </Button>
+      <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        onClick={handleDeleteUser}
+      >
+        Delete User Only Id
+      </Button>
     </div>
   );
 };
